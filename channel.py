ac46b8afad981f8e10ec29cb8c3d0facf065a823
Marina Berdnikova <berdni4ka@rambler.ru>
2023-02-26 02:24:40 +0300

    Add channel.py

--- channel.py
+++ channel.py
@@ -2,23 +2,20 @@ import json
 import os
 
 from googleapiclient.discovery import build
+from pprint import pprint
 
 
 class Channel:
-
-
-    # создать специальный объект для работы с API
-
 
     def __init__(self, id):
         self.id = id
 
     def _get_channel_info(self):
+        api_key: str = os.getenv('API KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.id, part='snippet,statistics').execute()
+        return pprint(json.dumps(channel, indent=2, ensure_ascii=False))
 
-        channel = Channel.youtube.channels().list(id=self.id, part='snippet,statistics').execute()
-        return channel
-api_key: str = os.getenv('YT_API_KEY')
-print(api_key is None)
-youtube = build('youtube', 'v3', developerKey=api_key)
+
 vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 vdud._get_channel_info()
